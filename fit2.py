22de3503d79b615eb1b4c4beb54539eef50fbf9b
karthikpeddi14 <128394123+karthikpeddi14@users.noreply.github.com>
2023-10-03 19:39:45 +0530

    Create fit2.py

---
+++ fit2.py
@@ -0,0 +1,12 @@
+# You imported DecisionTreeRegressor in your last exercise
+# and that code has been copied to the setup code above. So, no need to
+# import it again
+
+# Specify the model
+iowa_model = DecisionTreeRegressor(random_state=1)
+
+# Fit iowa_model with the training data.
+iowa_model.fit(train_X,train_y)
+
+# Check your answer
+step_2.check()
