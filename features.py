cdee4b832950d5b0e69fc0bedaae6ac30d6ad9cf
karthikpeddi14 <128394123+karthikpeddi14@users.noreply.github.com>
2023-10-03 19:24:12 +0530

    Create features.py

---
+++ features.py
@@ -0,0 +1,14 @@
+# Create the list of features below
+feature_names = ['LotArea'
+,'YearBuilt'
+,'1stFlrSF'
+,'2ndFlrSF'
+,'FullBath'
+,'BedroomAbvGr'
+,'TotRmsAbvGrd']
+
+# Select data corresponding to features in feature_names
+X = home_data[feature_names]
+
+# Check your answer
+step_2.check()
