95946a705835fe480f644266c1a454dc085790a0
karthikpeddi14 <128394123+karthikpeddi14@users.noreply.github.com>
2023-10-03 19:25:57 +0530

    Create fit.py

---
+++ fit.py
@@ -0,0 +1,10 @@
+ from sklearn.tree import DecisionTreeRegressor
+#specify the model. 
+#For model reproducibility, set a numeric value for random_state when specifying the model
+iowa_model = DecisionTreeRegressor(random_state=1)
+
+# Fit the model
+iowa_model.fit(X,y)
+
+# Check your answer
+step_3.check()
